65cbf431875cf1e2074b43b5e216b69aa63b3715
Yuka Yasuda <yasuda-yuka@outlook.jp>
2016-07-12 17:13:05 +0900

    ユーザー追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160706013825) do
+ActiveRecord::Schema.define(version: 20160712074933) do
 
   create_table "books", force: :cascade do |t|
     t.string   "isbn"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 20160706013825) do
   create_table "reviews", force: :cascade do |t|
     t.string   "book_id"
     t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "userid"
+    t.string   "username"
+    t.string   "password"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
