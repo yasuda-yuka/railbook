e7174890a9d0a8babf740b52fc26a3be2391e4be
Yuka Yasuda <yasuda-yuka@outlook.jp>
2016-09-14 16:00:36 +0900

    ログイン機能の追加-2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  skip_before_action :check_logined
   before_action :auth
 
   # GET /users
